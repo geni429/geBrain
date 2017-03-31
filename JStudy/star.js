5090af1fa0b4eb6d6591b35ef7822962cf437c1f
geni429 <geni429@gmail.com>
2017-03-31 22:52:40 +0900

    Start JavaScript Study

---
+++ JStudy/star.js
@@ -0,0 +1,13 @@
+function pyramid_1(){
+  for(var i = 0; i < 5; i++){
+    var star = "";
+    for(var j = 0; j < i+1; j++){
+      star += '*';
+    }
+    console.log(star + '\n');
+  }
+}
+
+function pyramid_2(){
+  
+}
